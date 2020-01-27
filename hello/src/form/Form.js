84c6a8ab09d62dcf77566072260d8273ad5a2649
HusmithaSilva <husmitha.silva0@gmail.com>
2020-01-27 10:49:03 +0530

    4th commit

--- hello/src/form/Form.js
+++ hello/src/form/Form.js
@@ -1,12 +1,60 @@
 import React, {Component} from 'react'
 
 class FormHandling extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            Username: '',
+            textarea: '',
+            topic: 'React'
+        }
+    }
+
+    handleUsername = (event) => {
+        this.setState({
+            Username: event.target.value
+        })
+    }
+
+    textvaluechange = (event) => {
+        this.setState({
+            textarea: event.target.value 
+        })
+    }
+
+    topicchange = (event) => {
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    submitchange = event => {
+        alert(`${this.state.Username} ${this.state.textarea} ${this.state.topic}`)
+    }
+
     render(){
         return(
-            <form>
+            <form onSubmit={this.submitchange}>
                 <div>
                     <label>Username</label>
-                    <input type='text'></input>
+                    <input type='text' value={this.state.Username} onChange={this.handleUsername}></input>
+                </div>
+                <div>
+                    <label>Comments</label>
+                    <textarea value={this.state.textarea} onChange={this.textvaluechange}></textarea>
+                </div>
+                <div>
+                    <label>Topics</label>
+                    <select value={this.state.topic} onChange={this.topicchange}>
+                        <option value='React'>React</option>
+                        <option value='vue'>vue</option>
+                        <option value='Angular'>Angular</option>
+                    </select>
+                </div>
+                <div>
+                    <button type='submit'>Submit</button>
                 </div>
             </form>
         )

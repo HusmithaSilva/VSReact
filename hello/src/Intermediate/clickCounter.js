9512a704e8f2e8dd8c066af2237c25990c457ba5
HusmithaSilva <husmitha.silva0@gmail.com>
2020-02-08 19:40:52 +0530

    commm

--- hello/src/Intermediate/clickCounter.js
+++ hello/src/Intermediate/clickCounter.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 
 class clickCounter extends Component{
     render(){
-        return <div> clicked x times </div>
+        return (<button>
+            click x times
+            </button>
+            )
                 
     }
 }
